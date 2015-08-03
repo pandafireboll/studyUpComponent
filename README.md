f8fbbabaeb8e9a3ffdc0908ed7c13d591e5ecb91
Kai Hsia <kaihsia@gmail.com>
2015-08-02 23:41:29 -0700

    Toggling

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ StudyUp Main Component 0.0.1
 Using React 0.13.0
 
 To build, run "npm start"
+
+Note: Does not work on chrome.  Please use firefox or safari. 
--- src/app.js
+++ src/app.js
@@ -1,11 +1,71 @@
-// Dummy Data
+	// Data Set
 	var data = [
 		{ id: 1, question:'Who is this?', answer:'Panda'},
 		{ id: 2, question:'Why are you here?', answer:'To make that green'},
 		{ id: 3, question:'What are you gonna do?', answer:'Keep coding!'}
 	];
 
-	var React = require('react'),
-		StudyUpApp = require('./StudyUpApp');
+	//Main Component
+	var StudyUpApp = React.createClass({
+		incrementId: function() {
+			this.setState({
+				id: this.state.id + 1
+			});
+		},
+		getInitialState: function() {
+        	return { showResults: false, id: 1 };
+    	},
+    	handleClick: function() {
+        	this.setState({ showResults: !this.state.showResults });
+    	},
+		render:function(){
+			return (
+					<div>
+						<h1>Secret Files</h1>
+						<Button localOnClick={this.handleClick} />
+                		{ this.state.showResults ? <StudyList data={this.props.data} /> : null }
+					</div>
+				)
+		}
+	});
+
+
+	//Button Component
+	var Button = React.createClass({
+		render:function(){
+			return (
+					<button onClick={this.props.localOnClick}>Toggle</button>
+				)
+		}
+	});
+
+	//Individual Card
+	var Card = React.createClass({
+		render:function(){
+			return (
+					<div>
+						<div>{this.props.id}</div>
+						<h3>{this.props.question}</h3>
+						<p>{this.props.answer}</p>
+					</div>
+				)
+		}
+	});
+
+	//List of Questions
+	var StudyList = React.createClass({
+		render:function(){
+					//Spits out each item in the array
+					var interview = this.props.data.map(function(round){
+						return <Card id={round.id} question={round.question} answer={round.answer} />
+					});
+					//Returns the entire data set
+					return (
+							<div>
+								{interview}
+							</div>
+						)
+		}
+	});
 
 	React.render(<StudyUpApp data={data} />, document.getElementById('content'))
